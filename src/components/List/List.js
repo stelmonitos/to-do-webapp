034c531bf1677de483b697889e24773de3bdcbec
Jakub Stelmasiak <stelmon3@gmail.com>
2024-02-05 22:38:59 +0100

    add useEffect

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,23 @@
 import Column from '../Column/Column';
 import styles from './List.module.scss';
+import { useState, useEffect } from 'react';
 
-const List = () => {
+const List = () => {   
+    const [columns, setColumns] = useState([
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ]); 
+    useEffect(() => {
+
+        setTimeout(() => {
+            setColumns([...columns, { id: 4, title: 'Test column'}]);
+        }, 2000);
+
+    }, []);
+  
+    
+
     return (
         <div className={styles.list}>
         <header className={styles.header}>
@@ -9,9 +25,7 @@ const List = () => {
         </header>
             <p className={styles.description}>Interesting things I want to check out</p>
             <section className={styles.columns}>
-                <Column title="Books" icon='book' />
-                <Column title="Movies" icon='film'/>
-                <Column title="Games" icon='gamepad'/>
+                {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
             </section>
         </div>
     );
