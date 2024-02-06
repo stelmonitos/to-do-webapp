601883a94b9fc7372d15d62cd065bd0d2df5e952
Jakub Stelmasiak <stelmon3@gmail.com>
2024-02-06 16:26:07 +0100

    add textinput

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,9 +1,17 @@
 import styles from './Column.module.scss';
+import Card from '../Card/Card';
+import CardForm from '../CardForm/CardForm';
 
 const Column = props => {
+    console.log(props.addCard)
     return (
         <article className={styles.column}>
             <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h2>
+            <ul className={styles.cards}>
+                {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+            </ul>
+            <CardForm columnId={props.id} action={props.addCard}/>
+           
         </article>
     )
 }
